31f73dc16d55753ab207f31274180959d292ab22
Kawan <kawan.gomes@sptech.school>
2024-11-15 00:07:26 -0300

    Estou quase desistindo desse quiz

---
+++ Site/questoes.js
@@ -0,0 +1,92 @@
+export default [
+    {
+      question: "Em que ano foi fundado o Corinthians?",
+      answers: [
+        { option: "1905", correct: false },
+        { option: "1910", correct: true },
+        { option: "1920", correct: false },
+        { option: "1920", correct: false },
+      ],
+    },
+    {
+      question: "Qual foi o adversário do Corinthians na final do Mundial de 2012?",
+      answers: [
+        { option: "Barcelona - ESP", correct: false },
+        { option: "Juventus - ITA", correct: false },
+        { option: "Bayern - ALE", correct: false },
+        { option: "Chelsea - ING", correct: true },
+      ],
+    },
+    {
+      question: "Quantos títulos brasileiros tem o Corinthians?",
+      answers: [
+        { option: "7", correct: true },
+        { option: "6", correct: false },
+        { option: "5", correct: false },
+        { option: "4", correct: false },
+      ],
+    },
+    {
+      question: "Qual o jogador com maior número de jogos pelo Corinthians?",
+      answers: [
+        { option: "Cássio", correct: false },
+        { option: "Zé Maria", correct: false },
+        { option: "Wladimir", correct: true },
+        { option: "Cláudio", correct: false },
+      ],
+    },
+    {
+        question: "Em que bairro de São Paulo é localizada a Neo Química Arena?",
+        answers: [
+          { option: "Itaquera", correct: true },
+          { option: "Canindé", correct: false },
+          { option: "Barra Funda", correct: false },
+          { option: "Morumbi", correct: false },
+        ],
+      },
+      {
+        question: "Qual o mascote do Corinthians?",
+        answers: [
+          { option: "Gambá", correct: false },
+          { option: "Gavião", correct: false },
+          { option: "Zizao", correct: false },
+          { option: "Mosqueteiro", correct: true },
+        ],
+      },
+      {
+        question: "Quem fez o gol da vitória contra o Vasco na Libertadores de 2012?",
+        answers: [
+          { option: "Fábio Santos", correct: false },
+          { option: "Paulinho", correct: true },
+          { option: "Emerson Sheik", correct: false },
+          { option: "Jorge Henrique", correct: false },
+        ],
+      },
+      {
+        question: "Como se inicia o hino do Corinthians?",
+        answers: [
+          { option: "Salve o Corinthians...", correct: true },
+          { option: "Até a pé nos iremos...", correct: false },
+          { option: "Eu teria um desgosto profundo...", correct: false },
+          { option: "Corinthians, Corinthians, goool...", correct: false },
+        ],
+      },
+      {
+        question: "Qual time inspirou a criação do Corinthians?",
+        answers: [
+          { option: "Fluminense", correct: false },
+          { option: "Manchester United", correct: false },
+          { option: "Corinthian FC", correct: true },
+          { option: "Criciúma", correct: false },
+        ],
+      },
+      {
+        question: "Qual estrangeiro mais vestiu a camisa do Corinthians?",
+        answers: [
+          { option: "Balbuena", correct: false },
+          { option: "Rincón", correct: false },
+          { option: "Guerrero", correct: false },
+          { option: "Romero", correct: true },
+        ],
+      },
+      ];
